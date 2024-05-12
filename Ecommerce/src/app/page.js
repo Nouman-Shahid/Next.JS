630efbb94d1073835e285472f3f75ccc5dd6a993
Nouman-Shahid <noumanshahid2486@gmail.com>
2024-05-12 11:17:10 +0500

    code refactoiring

---
+++ Ecommerce/src/app/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import Navbar from "./Components/Navbar";
+import Homee from "./Components/Homee";
+
+export default function Home() {
+  return (
+    <main>
+      <Navbar />
+      <Homee />
+    </main>
+  );
+}
