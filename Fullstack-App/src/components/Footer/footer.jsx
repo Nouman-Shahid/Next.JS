360cd8a01963a90f89eacdb4bf333e2abaa0dd3d
Nouman-Shahid <noumanshahid2486@gmail.com>
2024-05-21 19:36:09 +0500

    code refactoring

--- Fullstack-App/src/components/Footer/footer.jsx
+++ Fullstack-App/src/components/Footer/footer.jsx
@@ -1,21 +1,22 @@
+import Link from "next/link";
 import React from "react";
 
 const footer = () => {
   const icons = [
     {
-      path: "image/fb.png",
+      path: "image/icons-img/fb.png",
     },
     {
-      path: "image/insta.png",
+      path: "image/icons-img/insta.png",
     },
     {
-      path: "image/x.png",
+      path: "image/icons-img/x.png",
     },
     {
-      path: "image/li.png",
+      path: "image/icons-img/li.png",
     },
     {
-      path: "image/yt.png",
+      path: "image/icons-img/yt.png",
     },
   ];
 
@@ -23,14 +24,16 @@ const footer = () => {
     <footer className="flex flex-col w-screen h-36  justify-between px-10 py-4 items-center ">
       <hr className="w-[90%] m-auto bg-slate-500" />
 
-      <div className="flex space-x-5 items-end">
-        <p>You can follow us on : </p>
-        {icons.map((items) => (
-          <img
-            src={items.path}
-            className="cursor-pointer h-[3vh] hover:scale-125"
-          />
-        ))}
+      <div className="flex flex-col  items-center md:flex-row md:space-x-5">
+        <p className="text-white">You can follow us on : </p>
+        <div className="flex space-x-2 my-4">
+          {icons.map((items) => (
+            <img
+              src={items.path}
+              className="cursor-pointer h-[3vh] hover:scale-125"
+            />
+          ))}
+        </div>
       </div>
     </footer>
   );
