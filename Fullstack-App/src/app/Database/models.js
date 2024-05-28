65d181c1479bae26cad67a504b8892a56e923e89
Nouman-Shahid <noumanshahid2486@gmail.com>
2024-05-28 16:16:24 +0500

    code refactoring

--- Fullstack-App/src/app/Database/models.js
+++ Fullstack-App/src/app/Database/models.js
@@ -60,5 +60,5 @@ const postSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-export const User = mongoose.models.User || mongoose.model("User", userSchema);
-export const Post = mongoose.models.Post || mongoose.model("Post", postSchema);
+export const User = mongoose.models.User || mongoose.model("user", userSchema);
+export const Post = mongoose.models.Post || mongoose.model("post", postSchema);
