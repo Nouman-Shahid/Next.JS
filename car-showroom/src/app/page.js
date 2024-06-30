d1947161d5688d458f2223e92c27dc55bb5f976b
Nouman-Shahid <noumanshahid2486@gmail.com>
2024-06-30 17:08:27 +0500

    code refactoring

--- car-showroom/src/app/page.js
+++ car-showroom/src/app/page.js
@@ -3,7 +3,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      hello!
+      <div className="hero">
+        <h1 className="hero__title">hello!</h1>
+      </div>
     </main>
   );
 }
